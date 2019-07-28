95c19edd51cb4e33887385e9ad56067d3443267c
haoran li <haoran.li@thoughtworks.com>
2019-07-28 22:48:02 +0800

    feat(models/cart.js): complete del and add reducers

--- app/models/cart.js
+++ app/models/cart.js
@@ -2,8 +2,30 @@ export default {
   namespace: 'cart',
   state: {},
   reducers: {
-    updateState(state, { payload }) {
-      return { ...state, ...payload }
+    del(state, { payload }) {
+      const { name } = payload
+      const cart = { ...state }
+      const product = cart[name]
+      if (product && product.amount > 1) {
+        product.amount -= 1
+        cart[name] = product
+      } else {
+        delete cart[name]
+      }
+
+      return { ...cart }
+    },
+    add(state, { payload }) {
+      const { name, price } = payload
+      const cart = { ...state }
+      let product = cart[name]
+      if (product) {
+        product.amount += 1
+      } else {
+        product = { price, name, amount: 1 }
+      }
+      cart[name] = product
+      return { ...cart }
     },
   },
   effects: {},
