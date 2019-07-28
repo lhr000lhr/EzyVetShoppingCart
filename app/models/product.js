4af27063c23c496cca9552cd71250376b4156338
haoran li <haoran.li@thoughtworks.com>
2019-07-28 19:49:06 +0800

    test(product.test): update test

--- app/models/product.js
+++ app/models/product.js
@@ -1,25 +1,43 @@
 import { getProducts } from '../services/product'
+import { createAction } from '../utils'
 
 export default {
   namespace: 'product',
   state: {
     products: [],
+    refreshing: false,
   },
   reducers: {
     updateState(state, { payload }) {
       return { ...state, ...payload }
     },
+    refreshingStart(state, { payload }) {
+      return {
+        ...state,
+        ...payload,
+        refreshing: true,
+      }
+    },
+    refreshingEnd(state, { payload }) {
+      return {
+        ...state,
+        ...payload,
+        refreshing: false,
+      }
+    },
   },
   effects: {
     *query(_, { call, put }) {
+      yield put(createAction('refreshingStart')({}))
       const data = yield call(getProducts)
-
       yield put({
         type: 'updateState',
         payload: {
-          products: [...data],
+          products: data,
         },
       })
+
+      yield put(createAction('refreshingEnd')({}))
     },
   },
   subscriptions: {},
--- app/models/product.test.js
+++ app/models/product.test.js
@@ -8,10 +8,19 @@ describe('Model Product', () => {
     const { call, put } = effects
     const { query } = product.effects
     const generator = query({}, { call, put })
+    generator.next()
     let next = generator.next()
+
     expect(next.value).to.deep.equal(call(getProducts))
     const data = { success: true }
     next = generator.next(data)
-    expect(next.value).to.deep.equal(put({ type: 'updateState', payload: data }))
+    expect(next.value).to.deep.equal(
+      put({
+        type: 'updateState',
+        payload: {
+          products: data,
+        },
+      })
+    )
   })
 })
